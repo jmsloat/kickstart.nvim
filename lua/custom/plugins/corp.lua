771bf43ed4a8206f2d9997cebf51e96d6e915e05
James Sloat <jsloat@google.com>
2024-05-02 13:49:07 +0000

    add obsidian to corp

--- lua/custom/plugins/corp.lua
+++ lua/custom/plugins/corp.lua
@@ -1,7 +1,24 @@
 local iscorp = os.getenv 'CORP'
 
 if iscorp then
-  return {}
+  return {
+    {
+      'epwalsh/obsidian.nvim',
+      lazy = true,
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+      },
+      opts = {
+        workspaces = {
+          {
+            name = 'work',
+            path = '~/notes',
+          },
+        },
+      },
+      ft = 'markdown',
+    },
+  }
 else
   return {}
 end
